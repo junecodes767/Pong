e46e074bc66ba8bbd6cf993263b802f7bfb901ef
junecodes767 <junecodes767@gmail.com>
2025-03-03 18:04:27 -0400

    add first paddle location

--- paddle.py
+++ paddle.py
@@ -2,8 +2,14 @@ from turtle import Turtle
 
 class Paddle(Turtle):
     def __init__(self):
-        #
+        #creates the rectangular shape of the paddle
         paddle = Turtle()
-        paddle.shape("square")
+        paddle.penup()
+        paddle.color("white") # paddle color
+        paddle.shape("square") # paddle shape
         paddle.resizemode("user")
-        paddle.turtlesize(stretch_wid=3,stretch_len=1,outline=1)
+        paddle.turtlesize(stretch_wid=3,stretch_len=1,outline=2) #paddle resize
+        paddle.goto(x=500, y=0)
+        
+        
+        
