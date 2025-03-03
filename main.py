138ce8047167c6341228ac54034511394d134873
junecodes767 <junecodes767@gmail.com>
2025-03-03 17:10:18 -0400

    created the pong screen element

---
+++ main.py
@@ -0,0 +1,29 @@
+from turtle import Screen, Turtle
+
+#initalise the  screen object
+screen = Screen()
+screen.tracer(0)
+#set up the screen size
+screen.setup(width=1080, height=600)
+
+#set the background color
+screen.bgcolor("black")
+
+# initialise the turtle class
+dotted_line = Turtle()
+dotted_line.color("white")
+dotted_line.hideturtle()
+dotted_line.penup()
+dotted_line.goto(x=0,y=300)
+
+dotted_line.setheading(270)
+for _ in range(40):
+    dotted_line.forward(10) #go forward by 10 paces
+    dotted_line.pendown()
+    dotted_line.forward(5)
+    dotted_line.penup()
+
+screen.update()   
+    
+# to exit the screen
+screen.exitonclick()
