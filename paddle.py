cc3e03968cc06db7d362dfb780182eeca4670952
junecodes767 <junecodes767@gmail.com>
2025-03-03 20:42:51 -0400

    add second paddle

--- paddle.py
+++ paddle.py
@@ -9,9 +9,19 @@ class Paddle(Turtle):
         self.paddle.shape("square") # paddle shape
         self.paddle.shapesize(stretch_wid=5,stretch_len=1, ) #paddle resize
         self.paddle.goto(x=500, y=0)
+        self.paddle_2 = Turtle()
+        self.paddle_2.penup()
+        self.paddle_2.color("white") # paddle color
+        self.paddle_2.shape("square") # paddle shape
+        self.paddle_2.shapesize(stretch_wid=5,stretch_len=1, ) #paddle resize
+        self.paddle_2.goto(x=-500, y=0)
          
         
     def move_up(self):
-        self.paddle.setheading(90)
-        self.paddle.forward(10)
-        
+        new_ycor =self.paddle.ycor() + 20
+        self.paddle.goto(x=self.paddle.xcor(),y=new_ycor)
+      
+    def move_down(self):
+        new_ycor =self.paddle.ycor() - 20
+        self.paddle.goto(x=self.paddle.xcor(),y=new_ycor)
+           
