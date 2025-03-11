a4071ec421f6ba04ee4138599bfee48fc33bd271
junecodes767 <junecodes767@gmail.com>
2025-03-11 13:59:06 -0400

    remove self.paddle: was creating problems for the ball turtle to detect the paddle turtle

--- paddle.py
+++ paddle.py
@@ -1,27 +1,23 @@
 from turtle import Turtle
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self,position ):
+        super().__init__()
         #creates the rectangular shape of the paddle
-        self.paddle = Turtle()
-        self.paddle.penup()
-        self.paddle.color("white") # paddle color
-        self.paddle.shape("square") # paddle shape
-        self.paddle.shapesize(stretch_wid=5,stretch_len=1, ) #paddle resize
-        self.paddle.goto(x=500, y=0)
-        self.paddle_2 = Turtle()
-        self.paddle_2.penup()
-        self.paddle_2.color("white") # paddle color
-        self.paddle_2.shape("square") # paddle shape
-        self.paddle_2.shapesize(stretch_wid=5,stretch_len=1, ) #paddle resize
-        self.paddle_2.goto(x=-500, y=0)
+        self.penup()
+        self.color("white") # paddle color
+        self.shape("square") # paddle shape
+        self.shapesize(stretch_wid=5,stretch_len=1, ) #paddle resize
+        self.goto(position)
+        
+         
          
         
     def move_up(self):
-        new_ycor =self.paddle.ycor() + 20
-        self.paddle.goto(x=self.paddle.xcor(),y=new_ycor)
+        new_ycor =self.ycor() + 20
+        self.goto(x=self.xcor(),y=new_ycor)
       
     def move_down(self):
-        new_ycor =self.paddle.ycor() - 20
-        self.paddle.goto(x=self.paddle.xcor(),y=new_ycor)
+        new_ycor =self.ycor() - 20
+        self.goto(x=self.xcor(),y=new_ycor)
            
